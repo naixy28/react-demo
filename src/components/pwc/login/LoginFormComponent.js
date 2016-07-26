c5331d541e9f6a74c640a54d193304d3d4b527b0
wong.huang <wong.huang@pwc.com>
2016-07-26 16:51:50 +0800

    add code

--- src/components/pwc/login/LoginFormComponent.js
+++ src/components/pwc/login/LoginFormComponent.js
@@ -1,16 +1,32 @@
 'use strict';
 
 import React from 'react';
+import {findDOMNode} from 'react-dom';
 
 require('styles/pwc/login/LoginForm.scss');
 
 class LoginFormComponent extends React.Component {
+  doLogin(){
+    var inputEmail=findDOMNode(this.refs.inputEmail);
+    var inputPassword=findDOMNode(this.refs.inputPassword);
+    var checkboxRemember=findDOMNode(this.refs.checkboxRemember);
+    alert("email:"+inputEmail.value+"| password:"+inputPassword.value+" |checkboxRemember:"+checkboxRemember.value);
+  }
   render() {
     return (
       <div className="loginform-component">
         <form>
-          login Part
-          </form>
+          <h2>Welcome, Please log in</h2>
+          <input type="email" className="form-control" placeholder="Email address" ref="inputEmail"/>
+          <input type="password" className="form-control" placeholder="Password" ref="inputPassword"/>
+          <div className="remember-login">
+            <label >
+              <input type="checkbox" ref="checkboxRemember"/>
+              <span>Remember me</span>
+            </label>
+          </div>
+          <button type="button" className="btn btn-pwc-orange" onClick={()=>this.doLogin()}>Login</button>
+        </form>
 
       </div>
     );
